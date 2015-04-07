c4a8ea591dd2f8fd701785d4e3271d4ca952246c
David Collado Sela <bitomule@gmail.com>
2015-04-07 09:48:05 +0200

    test ready

--- collectionFSBug.js
+++ collectionFSBug.js
@@ -1,23 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    'change .myFileInput': function(event, template) {
+      var files = event.target.files;
+      for (var i = 0, ln = files.length; i < ln; i++) {
+        Files.insert(files[i], function (err, fileObj) {
+          // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+        });
+      }
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
